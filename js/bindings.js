9701043c916d207f70ce25deb3d953cd4bbb6ca8
Mattias Buelens <mattias.buelens@gmail.com>
2014-08-03 16:09:59 +0200

    Added no click and indeterminate bindings.

--- js/bindings.js
+++ js/bindings.js
@@ -14,3 +14,21 @@ ko.bindingHandlers.tppState = {
 			.prop("indeterminate", state === State2.MISS);
     }
 };
+
+// Disables clicking on an element
+ko.bindingHandlers.tppNoClick = {
+    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+		$(element).on("click", function(e) {
+			var value = ko.unwrap(valueAccessor());
+			if (value) e.preventDefault();
+		});
+    }
+};
+
+// Binding for indeterminate property on checkboxes
+ko.bindingHandlers.tppInd = {
+    update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var value = ko.unwrap(valueAccessor());
+		$(element).prop("indeterminate", value);
+    }
+};
